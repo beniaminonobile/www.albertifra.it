ca00b87ed2be4be9b0fd6f0f3efe07d25ea99eca
beniamino.nobile <beniamino.nobile@gmail.com>
2017-11-01 20:37:30 +0100

    sito in costruzione

--- web/urls.py
+++ web/urls.py
@@ -20,12 +20,13 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 
 
 urlpatterns = [
-
+    url(r'^', TemplateView.as_view(template_name='site/home.html'), name="homepage"),
     # ADMIN URL
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
